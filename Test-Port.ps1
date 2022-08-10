f6b70ae6422939f7136fbacac2c2c2ddf279cea9
AlexanderSchwarzppedvAG <alexanders@ppedv.de>
2022-08-10 08:40:09 +0200

    update1

---
+++ Test-Port.ps1
@@ -0,0 +1,9 @@
+﻿function Test-Port
+
+{$computer=Read-Host "Computername | Ziel-IP-Adresse?"
+
+$port=Read-Host "Portnummer? mehrere Ports durch Komma trennen"
+
+$port.split(',') | Foreach-Object -Process {If (($a=Test-NetConnection $computer -Port $_ -WarningAction SilentlyContinue).tcpTestSucceeded -eq $true) {Write-Host $a.Computername $a.RemotePort -ForegroundColor Green -Separator " jupp; "} else {Write-Host $a.Computername $a.RemotePort -Separator " nix; " -ForegroundColor Red}}
+
+}
